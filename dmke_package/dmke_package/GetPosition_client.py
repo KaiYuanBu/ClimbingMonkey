424e117d0359f7018bc372481ffc2667629658c4
KaiYuanBu <158999107+KaiYuanBu@users.noreply.github.com>
2024-03-12 23:11:26 +0800

    Add files via upload

---
+++ dmke_package/dmke_package/GetPosition_client.py
@@ -0,0 +1,41 @@
+import sys
+
+from dmke_interface.action import GetPosition
+import rclpy
+from rclpy.node import Node
+
+
+class GetPositionClient(Node):
+
+    def __init__(self):
+        super().__init__('get_position_client')
+        self.cli = self.create_client(GetPosition, 'get_position_client')
+        while not self.cli.wait_for_service(timeout_sec=10.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = GetPosition.Request()
+
+    def call_service(self):
+        self.get_logger().info('Calling the empty service...')
+        future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, future)
+        if future.result() is not None:
+            response = future.result()
+            if response.position:
+                self.get_logger().info(f'Position Obtained: {response}')
+            else:
+                self.get_logger().info('Service call failed!')
+        else:
+            self.get_logger().info('Service call failed: Timeout exceeded')
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    getpos_client = GetPositionClient()
+    getpos_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
